e1c047198ca985e3838ff86399908d91ff3e423c
Shane Knudsen <shaneknu@gmail.com>
2020-04-26 16:51:16 -0400

    Further eslint fixes

--- index.js
+++ index.js
@@ -19,25 +19,25 @@ async function inlineReferences(code, origin, quiet) {
 
   let root = Path.dirname(origin);
 
-  let nodes = Acorn.parse(code,{
+  let nodes = Acorn.parse(code, {
     ecmaVersion: 10,
     sourceType: 'module'
   });
 
   let offset = 0;
   let paths = AwesomeUtils.Object.paths(nodes);
-  await AwesomeUtils.Promise.series(paths,(path)=>{
+  await AwesomeUtils.Promise.series(paths, (path) => {
     if (!path) return;
 
-    return new Promise(async (resolve)=>{
+    return new Promise(async (resolve) => {
       try {
-        let node = AwesomeUtils.Object.get(nodes,path);
+        let node = AwesomeUtils.Object.get(nodes, path);
         if (typeof node==='object' && node.type && node.type==='CallExpression' && node.callee && node.callee.type==='Identifier') {
 
           let revised = null;
-          if (node.callee.name==='html') revised = await inlineHTML(root,code,offset,node,quiet);
-          else if (node.callee.name==='css') revised = await inlineCSS(root,code,offset,node,quiet);
-          else if (node.callee.name==='asset') revised = await inlineAsset(root,code,offset,node,quiet);
+          if (node.callee.name==='html') revised = await inlineHTML(root, code, offset, node, quiet);
+          else if (node.callee.name==='css') revised = await inlineCSS(root, code, offset, node, quiet);
+          else if (node.callee.name==='asset') revised = await inlineAsset(root, code, offset, node, quiet);
           if (revised) {
             code = revised.code;
             offset = revised.offset;
@@ -47,7 +47,7 @@ async function inlineReferences(code, origin, quiet) {
         resolve();
       }
       catch (ex) {
-        console.error('\nERROR: '+origin+': '+ex);
+        console.error(`\nERROR: ${origin}: ${ex}`);
         process.exit();
       }
     });
@@ -56,8 +56,8 @@ async function inlineReferences(code, origin, quiet) {
   return code;
 }
 
-function inlineHTML(root,code,offset,node,quiet) {
-  return new Promise(async (resolve,reject)=>{
+function inlineHTML(root, code, offset, node, quiet) {
+  return new Promise(async (resolve, reject) => {
     try {
       if (!node) return resolve();
 
@@ -90,23 +90,23 @@ function inlineHTML(root,code,offset,node,quiet) {
 
       let data = null;
       if (filename.startsWith('http:') || filename.startsWith('https:') || filename.startsWith('ftp:')) {
-        if (!quiet) console.log('  '+filename);
-        ({data} = await loader(filename));
+        if (!quiet) console.log(`  ${filename}`);
+        ({ data } = await loader(filename));
       }
       else {
         // data is not a filename and probably inline content.
-        filename = resolveFilename(root,filename,['.html','.htm']);
+        filename = resolveFilename(root, filename, [ '.html', '.htm' ]);
         if (!filename) return;
 
-        if (!quiet) console.log('  '+filename);
-        ({data} = await loader(filename));
+        if (!quiet) console.log(`  ${filename}`);
+        ({ data } = await loader(filename));
       }
-      if (data===undefined || data===null) return reject('Could not find inline reference to '+filename+'.');
+      if (data===undefined || data===null) return reject(`Could not find inline reference to ${filename}.`);
 
-      code = code.slice(0,start+offset)+'`'+data.replace(/`/g,'\\`')+'`'+code.slice(end+offset);
+      code = `${code.slice(0, start+offset)}\`${data.replace(/`/g, '\\`')}\`${code.slice(end+offset)}`;
       offset += (data.length-length);
 
-      resolve({code,offset});
+      resolve({ code, offset });
     }
     catch (ex) {
       return reject(ex);
@@ -114,8 +114,8 @@ function inlineHTML(root,code,offset,node,quiet) {
   });
 }
 
-function inlineCSS(root,code,offset,node,quiet) {
-  return new Promise(async (resolve,reject)=>{
+function inlineCSS(root, code, offset, node, quiet) {
+  return new Promise(async (resolve, reject) => {
     try {
       if (!node) return resolve();
 
@@ -147,23 +147,23 @@ function inlineCSS(root,code,offset,node,quiet) {
 
       let data = null;
       if (filename.startsWith('http:') || filename.startsWith('https:') || filename.startsWith('ftp:')) {
-        if (!quiet) console.log('  '+filename);
-        ({data} = await loader(filename));
+        if (!quiet) console.log(`  ${filename}`);
+        ({ data } = await loader(filename));
       }
       else {
         // data is not a filename and probably inline content.
-        filename = resolveFilename(root,filename,['.css']);
+        filename = resolveFilename(root, filename, [ '.css' ]);
         if (!filename) return;
 
-        if (!quiet) console.log('  '+filename);
-        ({data} = await loader(filename));
+        if (!quiet) console.log(`  ${filename}`);
+        ({ data } = await loader(filename));
       }
-      if (data===undefined || data===null) return reject('Could not find inline reference to '+filename+'.');
+      if (data===undefined || data===null) return reject(`Could not find inline reference to ${filename}.`);
 
-      code = code.slice(0,start+offset)+'`'+data.replace(/`/g,'\\`')+'`'+code.slice(end+offset);
+      code = `${code.slice(0, start+offset)}\`${data.replace(/`/g, '\\`')}\`${code.slice(end+offset)}`;
       offset += (data.length-length);
 
-      resolve({code,offset});
+      resolve({ code, offset });
     }
     catch (ex) {
       return reject(ex);
@@ -171,8 +171,8 @@ function inlineCSS(root,code,offset,node,quiet) {
   });
 }
 
-function inlineAsset(root,code,offset,node,quiet) {
-  return new Promise(async (resolve,reject)=>{
+function inlineAsset(root, code, offset, node, quiet) {
+  return new Promise(async (resolve, reject) => {
     try {
       if (!node) return resolve();
 
@@ -205,29 +205,29 @@ function inlineAsset(root,code,offset,node,quiet) {
       let data = null;
       let mimetype = null;
       if (filename.startsWith('http:') || filename.startsWith('https:') || filename.startsWith('ftp:')) {
-        if (!quiet) console.log('  '+filename);
-        ({data,mimetype} = await loader(filename,filename,null));
+        if (!quiet) console.log(`  ${filename}`);
+        ({ data, mimetype } = await loader(filename, filename, null));
       }
       else {
         // data is not a filename and probably inline content.
-        filename = resolveFilename(root,filename,extensions);
+        filename = resolveFilename(root, filename, extensions);
 
         if (!filename) return;
 
-        if (!quiet) console.log('  '+filename);
-        ({data,mimetype} = await loader(filename,filename,null));
+        if (!quiet) console.log(`  ${filename}`);
+        ({ data, mimetype } = await loader(filename, filename, null));
 
         if (!mimetype) mimetype = AwesomeUtils.MimeTypes.getTypeForExtension(Path.extname(filename));
       }
-      if (!data) return reject('Could not find inline reference to '+filename+'.');
+      if (!data) return reject(`Could not find inline reference to ${filename}.`);
 
-      if (!mimetype) return reject('asset() could not associate valid mime type with filename: '+filename);
-      data = 'data:'+mimetype+';base64,'+data.toString('base64');
+      if (!mimetype) return reject(`asset() could not associate valid mime type with filename: ${filename}`);
+      data = `data:${mimetype};base64,${data.toString('base64')}`;
 
-      code = code.slice(0,start+offset)+'`'+data+'`'+code.slice(end+offset);
+      code = `${code.slice(0, start+offset)}\`${data}\`${code.slice(end+offset)}`;
       offset += (data.length-length);
 
-      resolve({code,offset});
+      resolve({ code, offset });
     }
     catch (ex) {
       return reject(ex);
@@ -235,14 +235,14 @@ function inlineAsset(root,code,offset,node,quiet) {
   });
 }
 
-function loader(url,rootDir,encoding='utf-8') {
-  return new Promise(async (resolve,reject)=>{
+function loader(url, rootDir, encoding='utf-8') {
+  return new Promise(async (resolve, reject) => {
     try {
       if (url.startsWith('http:') || url.startsWith('https:') || url.startsWith('ftp:')) {
         let tried = [];
         let response = null;
         while (true) {
-          if (tried.indexOf(''+url)>-1) return reject('URL redirect created a circular reference and could not be resolved.');
+          if (tried.indexOf(`${url}`)>-1) return reject('URL redirect created a circular reference and could not be resolved.');
           tried.push(url);
 
           response = await AwesomeUtils.Request.get(url);
@@ -259,20 +259,20 @@ function loader(url,rootDir,encoding='utf-8') {
               return reject('URL did not return anything.');
             }
           }
-          else return reject('URL returned an error code: '+response.statusCode+': '+(response.status||''));
+          else return reject(`URL returned an error code: ${response.statusCode}: ${response.status||''}`);
         }
 
         if (!response) return reject('URL did not return anything.');
 
         let data = await response.content;
         let mimetype = response.contentType;
-        resolve({data,mimetype});
+        resolve({ data, mimetype });
       }
       else {
-        url = Path.resolve(rootDir,url);
-        FS.readFile(url,{
+        url = Path.resolve(rootDir, url);
+        FS.readFile(url, {
           encoding: encoding
-        },(err,data)=>{
+        }, (err, data) => {
           if (err) return reject(err);
           resolve({
             data,
@@ -287,23 +287,23 @@ function loader(url,rootDir,encoding='utf-8') {
   });
 }
 
-function resolveFilename(root,filename,extensions) {
+function resolveFilename(root, filename, extensions) {
   if (!root || !filename) return null;
 
-  let possible = Path.resolve(root,filename);
+  let possible = Path.resolve(root, filename);
   if (AwesomeUtils.FS.existsSync(possible)) return possible;
   if (!extensions) return null;
-  if (!(extensions instanceof Array)) extensions = [extensions];
+  if (!(extensions instanceof Array)) extensions = [ extensions ];
 
-  return extensions.reduce((possible,ext)=>{
+  return extensions.reduce((possible, ext) => {
     if (possible) return possible;
 
     if (!ext) return null;
-    if (!ext.startsWith('.')) ext = '.'+ext;
+    if (!ext.startsWith('.')) ext = `.${ext}`;
 
-    possible = Path.resolve(root,possible+ext);
+    possible = Path.resolve(root, possible+ext);
     if (AwesomeUtils.FS.existsSync(possible)) return possible;
 
     return null;
-  },null);
+  }, null);
 }
